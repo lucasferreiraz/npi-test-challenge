9ebe6075b3b4f96fd0e48d369ccb8a0490b58aa9
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-06-24 10:03:38 -0300

    feat: audit data for the socio entity

--- backend/src/main/java/com/challenge/npi/entities/Socio.java
+++ backend/src/main/java/com/challenge/npi/entities/Socio.java
@@ -1,9 +1,14 @@
 package com.challenge.npi.entities;
 
+import java.time.Instant;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +22,12 @@ public class Socio {
     private String nome;
     private Double renda;
     private Boolean ativo;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Socio() {
     }
@@ -58,6 +69,24 @@ public class Socio {
 
     public void setAtivo(Boolean ativo) {
         this.ativo = ativo;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
