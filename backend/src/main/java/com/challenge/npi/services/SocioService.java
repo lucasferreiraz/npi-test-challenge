27d6d2db4b817d4986970c7a54c16eedc6205b26
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-06-24 00:55:32 -0300

    feat: socio service

---
+++ backend/src/main/java/com/challenge/npi/services/SocioService.java
@@ -0,0 +1,59 @@
+package com.challenge.npi.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.challenge.npi.dtos.SocioDTO;
+import com.challenge.npi.entities.Socio;
+import com.challenge.npi.repositories.SocioRepository;
+
+@Service
+public class SocioService {
+    
+    @Autowired
+    private SocioRepository socioRepository;
+
+    @Transactional(readOnly = true)
+    public Page<SocioDTO> findAllPaged(PageRequest PageRequest) {
+        Page<Socio> list = socioRepository.findAll(PageRequest);
+        return list.map(socio -> new SocioDTO(socio));
+    }
+    
+    @Transactional(readOnly = true)
+    public SocioDTO findById(Long id) {
+        Optional<Socio> optional = socioRepository.findById(id);
+        return new SocioDTO(optional.get());
+    }
+
+    public SocioDTO insert(SocioDTO dto) {
+        Socio socio = new Socio();
+        copyDtoToEntity(dto, socio);
+
+        socio = socioRepository.save(socio);
+        return new SocioDTO(socio);
+    }
+
+    public SocioDTO update(Long id, SocioDTO dto) {
+        Socio socio = socioRepository.getReferenceById(id);
+        copyDtoToEntity(dto, socio);
+
+        socio = socioRepository.save(socio);
+        return new SocioDTO(socio);
+    }
+
+    public void delete(Long id) {
+        socioRepository.deleteById(id);
+    }
+
+    private void copyDtoToEntity(SocioDTO dto, Socio entity) {
+        entity.setNome(dto.getNome());
+        entity.setRenda(dto.getRenda());
+        entity.setAtivo(dto.getAtivo());
+    }
+
+}
