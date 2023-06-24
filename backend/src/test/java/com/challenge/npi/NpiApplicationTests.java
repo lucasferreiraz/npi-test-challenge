f40926820ea8c425f1239c7c4abc8ee15ad67959
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-06-23 23:11:10 -0300

    chore: initialized spring boot project

---
+++ backend/src/test/java/com/challenge/npi/NpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.npi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
