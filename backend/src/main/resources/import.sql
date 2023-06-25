11ea53a87d86059545da4adade926fa2ece3f6f8
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-06-24 21:27:26 -0300

    feat: seeding database

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_socio (nome, renda, ativo) VALUES ('João Silva', 1000.0, true);
+INSERT INTO tb_socio (nome, renda, ativo) VALUES ('Maria Oliveira', 2000.0, true);
+
+INSERT INTO tb_dependente (nome, idade, socio_id) VALUES ('Pedro Silva', 10, 1);
+INSERT INTO tb_dependente (nome, idade, socio_id) VALUES ('Maria Silva', 8, 1);
+INSERT INTO tb_dependente (nome, idade, socio_id) VALUES ('Carlos Silva', 12, 1);
+
+INSERT INTO tb_dependente (nome, idade, socio_id) VALUES ('Ana Oliveira', 6, 2);
+INSERT INTO tb_dependente (nome, idade, socio_id) VALUES ('Luiza Oliveira', 4, 2);
+INSERT INTO tb_dependente (nome, idade, socio_id) VALUES ('Pedro Oliveira', 8, 2);
+
