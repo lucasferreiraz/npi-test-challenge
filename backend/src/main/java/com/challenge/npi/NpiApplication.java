1b09a8597a0fd37968411fe68bde93b385addf8e
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-06-24 21:50:57 -0300

    feat: add swagger for api documentation

--- backend/src/main/java/com/challenge/npi/NpiApplication.java
+++ backend/src/main/java/com/challenge/npi/NpiApplication.java
@@ -3,7 +3,19 @@ package com.challenge.npi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "API de Gerenciamento de Sócios e Dependentes",
+		version = "1.0",
+		description = "Documentando uma API básica de gerenciamento de sócios e dependentes",
+		contact = @Contact(name = "Lucas Ferreira", email = "ferreiralucas090@gmail.com", url = "https://github.com/lucasferreiraz")
+	)
+)
 public class NpiApplication {
 
 	public static void main(String[] args) {
